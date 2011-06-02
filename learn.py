127afaba8a9419e96f36073cc135d9ce1a6f3d13
Chris Testa <testa@whoplans.org>
2011-06-01 23:06:10 -0700

    Some cleanup of learn.py

--- learn.py
+++ learn.py
@@ -10,16 +10,20 @@ import rdflib
 DEFAULT_DATA_DIR = 'data'
 
 def main():
+    # DBPedia's definition of a Plant.
     DBPEDIA_PLANT_URI = 'http://dbpedia.org/resource/Plant'
+    # Setting up the global SPARQL namespace.
     NS = {
         'dbpedia-owl': rdflib.Namespace('http://dbpedia.org/ontology/'),
     }
 
+    # A few magic incantations to turn on SPARQL in rdflib.
     rdflib.plugin.register('sparql', rdflib.query.Processor,
                            'rdfextras.sparql.processor', 'Processor')
     rdflib.plugin.register('sparql', rdflib.query.Result,
                            'rdfextras.sparql.query', 'SPARQLQueryResult')
 
+    # Our graph of plant information.
     g = rdflib.Graph()
 
     # Start by crawling the Plant fact file.
@@ -28,14 +32,14 @@ def main():
 
     # For each entity that is in the kingom of plants, learn facts about that
     # plant.
-    #for plant_uri in g.query('SELECT ?s WHERE { ?s dbpedia-owl:kingdom ?o }',
-    #        initNs=NS):
-    #    print 'Learning about %s ...' % plant_uri
-    #    g.parse(plant_uri)
+    for plant_uri in g.query('SELECT ?s WHERE { ?s dbpedia-owl:kingdom ?o }',
+            initNs=NS):
+        print 'Learning about %s ...' % plant_uri
+        g.parse(plant_uri)
 
     print 'Learned %d facts.' % len(g)
 
-    # Write down what we've learned!
+    # Write down what we've learned so that way we don't have to recrawl it!
     filename = os.path.join(os.environ.get('DATA_DIR', DEFAULT_DATA_DIR),
                 'plants_from_dbpedia.rdf')
     print 'Writing facts down in %s ...' % filename
